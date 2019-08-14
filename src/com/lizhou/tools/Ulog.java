5a712a275724e7fdb93b8521264f039b2160ceb3
hjj <474664736@qq.com>
2019-08-14 17:14:27 +0800

    初次提交

--- src/com/lizhou/tools/Ulog.java
+++ src/com/lizhou/tools/Ulog.java
@@ -1,11 +1,14 @@
 package com.lizhou.tools;
 
 public class Ulog {
-    public static void i(String tag, String content) {
+
+    public static void i(String tag, Object content) {
         System.out.println(tag + "---" + content + "");
     }
 
-    public static void i(String content) {
+    public static void i(Object content) {
         System.out.println("---" + content + "");
     }
+
+
 }
